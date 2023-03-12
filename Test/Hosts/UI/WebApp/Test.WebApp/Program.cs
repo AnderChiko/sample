79307e827b6ca9fe97404249ea7851a112707bd6
Anderson <chikoanderson@gmail.com>
2023-03-12 04:34:33 +0200

    updated web app dependancy injection

--- Test/Hosts/UI/WebApp/Test.WebApp/Program.cs
+++ Test/Hosts/UI/WebApp/Test.WebApp/Program.cs
@@ -1,8 +1,13 @@
+using Test.WebApp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddServices();
+
 
 var app = builder.Build();
 
